6c4ae74d038b771ea7d861a6259ce28e530cdfa3
mrugerio <mrugerio@gmail.com>
2018-01-03 16:40:59 -0600

    grails create-domain-class com.optica.NotaVenta

---
+++ mod-domain/grails-app/domain/com/optica/NotaVenta.groovy
@@ -0,0 +1,7 @@
+package com.optica
+
+class NotaVenta {
+
+    static constraints = {
+    }
+}
---
+++ mod-domain/src/test/groovy/com/optica/NotaVentaSpec.groovy
@@ -0,0 +1,18 @@
+package com.optica
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class NotaVentaSpec extends Specification implements DomainUnitTest<NotaVenta> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
