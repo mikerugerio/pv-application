ba9fcac8f7d01542c5bddf78684724d7b9073833
mrugerio <mrugerio@gmail.com>
2018-01-05 17:19:06 -0600

    Se crearon roles y usuarios al iniciar la aplicación.

--- app-admin/grails-app/init/app/admin/BootStrap.groovy
+++ app-admin/grails-app/init/app/admin/BootStrap.groovy
@@ -1,9 +1,41 @@
 package app.admin
 
+import app.admin.security.Role
+import app.admin.security.User
+import app.admin.security.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+    	populateUsers()
     }
+
+
+    def populateUsers() {
+        if(User.count() == 0) {
+            def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+            def operatorRole = new Role(authority: 'ROLE_OPERATOR').save()
+            def customerRole = new Role(authority: 'ROLE_CUSTOMER').save()
+
+            def adminUser = new User(username: 'admin', password: 'Password123!').save()
+            def operatorUser = new User(username: 'operator', password: 'Password123!').save()
+            def customerUser = new User(username: 'customer', password: 'Password123!').save()
+
+            UserRole.create adminUser, adminRole
+            UserRole.create operatorUser, operatorRole
+            UserRole.create customerUser, customerRole
+
+            UserRole.withSession {
+                it.flush()
+                it.clear()
+            }
+        }
+
+        assert Role.count() == 3
+		//assert User.count() == 3
+		//assert UserRole.count() == 3
+    }
+
     def destroy = {
     }
 }
